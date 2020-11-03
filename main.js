3718fdfc9b3001e571545c47708ff3b6f9f766f5
AranguZ <72930772+AranguZ@users.noreply.github.com>
2020-11-03 19:52:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Copyright 2020
 // DHBW Lörrach, Programmieren Abschlussprojekt: Hauswirtschaftsprogramm
-// David Schüler <Mail> & Christian Künzel <kunibertgames@web.de>
+// David Schüler <david.schueler97@gmail.com> & Christian Künzel <kunibertgames@web.de>
 // Matr.Nr & Matr. Nr
 //
 // Main Datei
