0d78731f6dd876ff95f78e5dfe1a68b5e43e9dd3
Christian Künzel <73838333+ChristianKuenzel@users.noreply.github.com>
2020-11-03 15:33:26 +0100

    Update structures.js

--- structures.js
+++ structures.js
@@ -1,1 +1,15 @@
-uff
+// Copyright 2020
+// DHBW Lörrach, Programmieren Abschlussprojekt: Hauswirtschaftsprogramm
+// David Schüler <Mail> & Christian Künzel <kunibertgames@web.de>
+// Matr.Nr & Matr. Nr
+//
+// Struktur Datei
+//
+// 1) const var
+// 2) var
+// 3) objects
+// 4) functions
+//
+// ______________________________________________________________________________________________
+
+// Begin
