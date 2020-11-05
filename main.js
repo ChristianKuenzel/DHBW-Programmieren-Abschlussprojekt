9c6fad48ec6b9d1f1c18fab24942de4f9c4c9a5a
Kuenzel Christian <ckuenzel@freicon.de>
2020-11-06 00:06:51 +0100

    fuck you js

--- main.js
+++ main.js
@@ -19,8 +19,57 @@ entryStorage = struct.initStorage[1];
 // Reading command line arguments; Initialize test mode for examination.
 let _ = struct.readCLA;
 
+
+
+
+
+// Introduction
+console.log("------------------------------------------------------------" +
+    "----------"); // 60 + 10 "-"
+console.log("Welcome to your budget software to manage your households finances!");
+console.log("------------------------------------------------------------" +
+    "----------"); // 60 + 10 "-"
+
 // Choose User
-    // Implement pw ?
+const prompt = require('prompt');
+prompt.start();
+
+var controller = new Boolean("true");
+
+while (controller == true) {
+    var inputNumber
+
+    console.log("Please choose one of the following by entering its number:");
+    console.log("1 - Create a new profil.");
+    console.log("2 - Choose an already existing profil.");
+    console.log("");
+    console.log("Input:");
+
+    prompt.get(['input'], function (err, result) {
+        inputNumber = parseInt(result.input);
+        console.log(inputNumber);
+
+        // if ()
+    });
+}
+
+
+// Text
+// User Input
+// Validate Input
+// if x -> repeat; y -> continue
+// !Safe input!
+
+// var = storage
+// while
+//// Text
+//// User Input
+//// if Input
+//// x -> repeat
+//// y -> var = input; break
+
+
+
 
 // Show start menu.
     // @David
